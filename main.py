88661250abeaaeb79f5f45da71ef9bc4ad5cd061
Renee Desporte <desporterenee@gmail.com>
2025-04-03 21:04:29 -0700

    Update main to reflect new functionality

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 """Main program functionality, including interactions with users."""
 
-from pitch_detector.utils import record, extract_pitch
-import matplotlib.pyplot as plt
-import numpy as np
+from pitch_detector.utils import record, extract_pitch, frequency_to_note
 
 if __name__ == "__main__":
 
     SAMPLE_RATE = 44100
-    DURATION = 5
+    DURATION = 3
     data = record(DURATION, SAMPLE_RATE)
 
-    plt.plot(np.linspace(0, DURATION, len(data)), data)
-    plt.title("Timeseries")
-    plt.xlabel("Time (s)")
-    plt.tight_layout()
-    plt.show()
-
     pitches = extract_pitch(data)
-    print(f"There were {len(pitches)} peak pitches found: {pitches}.")
+    print(f"There were {len(pitches)} peak pitches found.")
+
+    notes = []
+    octaves = []
+    for pitch in pitches:
+        note, octave = frequency_to_note(pitch)
+        notes.append(note)
+        octaves.append(octave)
+
+    print(
+        f"The most prominent frequency was {pitches[0]} Hz, "
+        f"which is a {notes[0]} note in octave {octaves[0]}."
+    )
