e463daa05ebd4a06accacb17c62f4ca5f32f9e13
Renee Desporte <desporterenee@gmail.com>
2025-04-28 20:08:12 -0700

    Absolute value should encompass total expression

--- src/pitch_detector/utils.py
+++ src/pitch_detector/utils.py
@@ -112,6 +112,6 @@ def frequency_to_note(freq):
     # Find octave
     freq = math.log2(freq)
     octave = round(abs(freq - MIDDLE))
-    scores = [octave - abs(freq - f) for f in MUSICAL_NOTES.values()]
+    scores = [abs(octave - freq - f) for f in MUSICAL_NOTES.values()]
     note = list(MUSICAL_NOTES.keys())[scores.index(min(scores))]
     return note, octave
