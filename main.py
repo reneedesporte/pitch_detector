3257811cc04994bb0146b2dc6a6053e3cd2fce2e
Renee Desporte <desporterenee@gmail.com>
2025-05-03 17:26:25 -0700

    Add initial buffer functionality for continuous recording

--- main.py
+++ main.py
@@ -1,26 +1,46 @@
 """Main program functionality, including interactions with users."""
 
-from pitch_detector.utils import record, extract_pitch, frequency_to_note
+import time
+import sounddevice as sd
+from pitch_detector.log import Logger
+from pitch_detector.buffer import Buffer
+
+log = Logger()
+buffer = Buffer(log)  # TODO does this need to be global?
+
+def callback(indata, frames, time, status):
+    """
+    Main callback function for audio data.
+
+    See Also
+    --------
+    sounddevice.InputStream
+    """
+    if any(indata):
+        data = indata
+        buffer.put(data)
 
 if __name__ == "__main__":
 
     SAMPLE_RATE = 44100
     DURATION = 3
-    data = record(DURATION, SAMPLE_RATE)
+    input_device_index, _ = sd.default.device
+    devices = sd.query_devices()
 
-    pitches = extract_pitch(data)
-    if len(pitches) == 0:
-        print(f"There were {len(pitches)} peak pitches found.")
-        exit()
-
-    notes = []
-    octaves = []
-    for pitch in pitches:
-        note, octave = frequency_to_note(pitch)
-        notes.append(note)
-        octaves.append(octave)
-
-    print(
-        f"The most prominent frequency was {pitches[0]} Hz, "
-        f"which is approximately an {notes[0]} note in octave {octaves[0]}."
+    log.write(f"Recording on '{devices[input_device_index]['name']}'!")
+    stream = sd.InputStream(
+        device=input_device_index, channels=1,
+        samplerate=SAMPLE_RATE, callback=callback
     )
+
+    stream.start()
+
+    try:
+        buffer.process()
+    except KeyboardInterrupt:
+        log.write("Quitting at user request.")
+    except Exception as e:
+        log.write(f"Exiting due to exception: {e}!")
+
+    stream.stop()
+    log.shutdown()
---
+++ src/pitch_detector/buffer.py
@@ -0,0 +1,95 @@
+"""Module containing classes and methods for the audio data buffer."""
+
+import time
+import numpy as np
+from pitch_detector import utils
+
+
+class Buffer:
+    """
+    Audio buffer class, which holds and processes audio data.
+
+    NOTE: Only 1-dimensional (single channel) buffer supported
+     at this time.
+
+    Attributes
+    ----------
+    logger : pitch_detector.log.Logger
+        Logger for file and terminal.
+    size : int
+        Size of 1-dimensional array (buffer).
+    data : ndarray
+        Circular buffer containing audio data.
+    fill_index : int
+        Index at which filling occurs.
+    process_index : int
+        Index at which processing occurs.
+    ramp_up : int
+        Count for initial filling of `data` (buffer).
+         Once `ramp_up` is large enough, processing begins.
+    PROCESSING_SAMPLE_RATE : int, constant
+        The number of sample points needed for processing.
+         Related to `ramp_up`.
+    sample_rate : int, default=44100
+        Audio sample rate
+    """
+    def __init__(self, logger, size=88200, sample_rate=44100):
+        """
+        Parameters
+        ----------
+        logger : log.Logger
+            Logger for file and terminal.
+        size : int, default=88200
+            Length of 1-dimensional input data.
+        sample_rate : int, default=44100
+            Audio sample rate.
+        """
+        self.logger = logger
+        self.size = size
+        self.data = np.zeros((size))
+        self.fill_index = 0
+        self.process_index = 0
+        self.ramp_up = 0
+        self.PROCESSING_SAMPLE_RATE = (
+            44100//2
+        )
+        self.sample_rate = sample_rate
+
+    def alert(self, note):
+        """Alert user of processing result."""
+        self.logger.write(f"Note: {note}")
+    
+    def put(self, array):
+        """Insert `array` into `data`."""
+        array = array.flatten()
+        for a in array:
+            self.fill_index += 1
+            if self.fill_index == self.size:
+                self.fill_index = 0
+            self.data[self.fill_index] = a
+        if self.ramp_up < self.size:
+            self.ramp_up += len(array)
+
+    def process(self):
+        """Perform signal processing, once ramp up period ends."""
+        while True:
+            time.sleep(0.5)
+            if self.ramp_up >= self.PROCESSING_SAMPLE_RATE:
+                self.logger.write("Starting processing!")
+                break
+        while True:
+            data = []
+            for _ in range(self.PROCESSING_SAMPLE_RATE):
+                self.process_index += 1
+                if self.process_index == self.size:
+                    self.process_index = 0
+                data.append(self.data[self.process_index])
+            pitches = utils.extract_pitch(
+                np.asarray(data),
+                self.sample_rate
+            )
+            if len(pitches) == 0:
+                self.alert("--")
+                continue
+            note, _ = utils.frequency_to_note(pitches[0])
+            self.alert(note)
